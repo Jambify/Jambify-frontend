ff0b0f6520c840b70acb923b1b716badadfc27fd
PETER <ezinwa.ugochukw@gmail.com>
2026-05-18 20:00:31 +0100

    feat: update build scripts and add Vercel configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), 
     tailwindcss()],
+    base: './', // Important for Vercel
+    build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 })
